c3caded5b6549b4de53d6acbaa3ecbcd95752d24
Priyanka Padinam <padinampriya@gmail.com>
2023-02-20 02:42:52 -0500

    ghhh

--- public/tuiter/HomeScreen/home.js
+++ public/tuiter/HomeScreen/home.js
@@ -7,7 +7,6 @@ function exploreComponent() {
        <!--h2>Explore</h2-->
   <div class="row mt-2">
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-    <h3>NavigationSidebar</h3>
     ${NavigationSidebar("Home")}
    </div>
    <div class="col-10 col-lg-7 col-xl-6">
